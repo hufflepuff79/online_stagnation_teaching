001f258ba1bf8bba361114b037cf176f84d7c41d
LuggiStruggi <lukasmkoenig@gmx.net>
2021-06-18 11:37:50 +0200

    network alpha weights

--- offline_reinforcement/agent/networks.py
+++ offline_reinforcement/agent/networks.py
@@ -4,8 +4,10 @@ import torch.nn.functional as F
 
 class REM(nn.Module):
 
-    def __init__(self, num_actions=18):
+    def __init__(self, num_actions=18, num_heads=200):
         super(REM, self).__init__()
+
+        self.num_heads = num_heads
 
         self.conv1 = nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
@@ -13,13 +15,16 @@ class REM(nn.Module):
 
         self.lin1 = nn.Linear(in_features=3136, out_features=512)
         
-        self.heads = [nn.Linear(in_features=512, out_features=num_actions) for i in range(200)]
+        self.heads = [nn.Linear(in_features=512, out_features=num_actions) for i in range(num_heads)]
 
         self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
 
 
-    def forward(self, x):
+    def forward(self, x, alphas):
+
+        if len(alphas) != self.num_heads:
+            raise ValueError("alphas need too be of same length as network heads")
 
         x = self.conv1(x)
         x = self.relu(x)
@@ -31,4 +36,15 @@ class REM(nn.Module):
         x = self.lin1(x)
         x = self.relu(x)
 
-        return [lin(x) for lin in self.heads]
+        return sum([alp*lin(x) for lin, alp in zip(self.heads, alphas)])
+
+
+
+if __name__ == "__main__":
+
+    x = torch.ones(size=(2, 4, 84, 84))
+    alphas = [0]*200
+    net = REM()
+    out = net(x, alphas)
+    print(out)
+    
