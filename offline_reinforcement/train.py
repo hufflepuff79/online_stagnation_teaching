cb685c4eb9e9738a9931e371e868599f75b697c4
hufflepuff79 <nina.pant@jupiter.uni-freiburg.de>
2021-06-21 14:26:34 +0200

    online validation function

--- offline_reinforcement/train.py
+++ offline_reinforcement/train.py
@@ -18,21 +18,34 @@ def train(atari_game, data_dir, epochs):
 
     # initiate training
     for epoch in range(epochs):
-        for iteration in range(iterations):  #TODO: how many iterations per epoch?
+        for iteration in range(1):  #TODO: how many iterations per epoch?
             agent.train_batch()
         # TODO: when to update the Q target network
         agent.update_target()
 
         # TODO: environment setup and online validation
-        agent.act()
+        validation_reward = online_validation(agent=agent, env=env)
 
         # TODO: track stats using tensorboard (needs to be added to Agent file)
+
+
+def online_validation(agent, env):
+    total_reward = 0
+    step_count = 0
+    state, reward, done, _ = env.reset()
+
+    while not done:
+        action = agent.act(state, deterministic=False)
+        state, reward, done, _ = env.step(action)  #TODO: does the input have to be a tuple
+        total_reward += reward
+        step_count += 1
+    return total_reward / step_count
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Parser to Initiate Agent Training")
     parser.add_argument('--data_dir', type=str, help='location of training data')
-    parser.add_arguement('--epochs', type=int, help='amount of epochs for training run', default=100)
+    parser.add_arguement('--epochs', type=int, help='amount of epochs for training run', default=1)
     parser.add_argument('--game', type=str, help='Atari game to train Agent on', default='Pong-v0')
 
     # TODO: additional argument for how often validation is performed/Q_target update is performed?
