514ece4fd7675577efa85001f6bca1489f720ec3
Florian Diederichs <florian.diederichs@neptun.uni-freiburg.de>
2021-06-25 12:19:36 +0200

    added device to rep_buffer

--- offline_reinforcement/agent/replay_buffer.py
+++ offline_reinforcement/agent/replay_buffer.py
@@ -8,6 +8,8 @@ STORE_FILENAME_PREFIX = '$store$_'
 
 ELEMS = ['observation', 'action', 'reward', 'terminal']
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class ReplayBuffer():
 
     def __init__(self, buffer_path) -> None:
@@ -22,11 +24,11 @@ class ReplayBuffer():
                     self.data[elem] = np.load(infile)
         
     def get_minibatch(self, batch_size: int = 32):
-        batch_state = torch.empty(batch_size, 4, 84, 84, dtype=torch.float32)
-        batch_next_state = torch.empty(batch_size, 4, 84, 84, dtype=torch.float32)
-        batch_actions = torch.empty(batch_size, 1, dtype=torch.long)
-        batch_reward = torch.empty(batch_size, 1, dtype=torch.float32)
-        batch_done = torch.empty(batch_size, 1, dtype=torch.int)
+        batch_state = torch.empty(batch_size, 4, 84, 84, dtype=torch.float32).to(device)
+        batch_next_state = torch.empty(batch_size, 4, 84, 84, dtype=torch.float32).to(device)
+        batch_actions = torch.empty(batch_size, 1, dtype=torch.long).to(device)
+        batch_reward = torch.empty(batch_size, 1, dtype=torch.float32).to(device)
+        batch_done = torch.empty(batch_size, 1, dtype=torch.int).to(device)
         for index, rand_index in enumerate(np.random.choice(len(self.data['action']) - 5, size=batch_size, replace=False)):
             batch_state[index, :, :, :] = torch.from_numpy(self.data['observation'][rand_index: rand_index+4, :, :])
             batch_next_state[index, :, :, :] = torch.from_numpy(self.data['observation'][rand_index+1: rand_index+5, :, :])
@@ -46,3 +48,18 @@ class ReplayBuffer():
             with open(filename, 'rb') as f:
                 with gzip.GzipFile(fileobj=f) as infile:
                     self.data[elem] = np.load(infile)
+
+    def get_static_minibatch(self, batch_size: int = 32):
+        batch_state = torch.empty(batch_size, 4, 84, 84, dtype=torch.float32)
+        batch_next_state = torch.empty(batch_size, 4, 84, 84, dtype=torch.float32)
+        batch_actions = torch.empty(batch_size, 1, dtype=torch.long)
+        batch_reward = torch.empty(batch_size, 1, dtype=torch.float32)
+        batch_done = torch.empty(batch_size, 1, dtype=torch.int)
+        for index, rand_index in enumerate(range(batch_size)):
+            batch_state[index, :, :, :] = torch.from_numpy(self.data['observation'][rand_index: rand_index+4, :, :])
+            batch_next_state[index, :, :, :] = torch.from_numpy(self.data['observation'][rand_index+1: rand_index+5, :, :])
+            batch_actions[index, :] = self.data['action'][rand_index+4]
+            batch_reward[index, :] = torch.from_numpy(np.asarray(self.data['reward'][rand_index+4]))
+            batch_done[index, :] = self.data['terminal'][rand_index+4]
+
+        return batch_state, batch_actions, batch_reward, batch_next_state, batch_done
