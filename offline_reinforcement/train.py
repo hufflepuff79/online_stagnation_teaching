736e07761498c501e206900b8252aaf88523332c
LuggiStruggi <lukasmkoenig@gmx.net>
2021-06-30 17:01:02 +0200

    codestyle train.py

--- offline_reinforcement/train.py
+++ offline_reinforcement/train.py
@@ -16,6 +16,7 @@ import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 def train(params):
 
     # create a summary writer for logging stats
@@ -25,16 +26,20 @@ def train(params):
     writer = SummaryWriter(log_dir=log_dir)
 
     # create Atari game environment
-    env = al.create_atari_environment(params.game, sticky_actions=params.env_sticky_actions)  # uses sticky actions as default
+    env = al.create_atari_environment(params.game, sticky_actions=params.env_sticky_actions)
     num_actions = env.action_space.n
 
     # create the Q network and Q target network
-    Q_network = REM(num_actions=num_actions, num_heads=params.model_num_heads, agent_history=params.agent_history)  #TODO: what values to give for num_actions, num_heads
-    Q_target_network = REM(num_actions=num_actions, num_heads=params.model_num_heads, agent_history=params.agent_history)  #TODO: what values to give for num_actions, num_heads
+    Q_network = REM(num_actions=num_actions,
+                    num_heads=params.model_num_heads, agent_history=params.agent_history)
+
+    Q_target_network = REM(num_actions=num_actions,
+                           num_heads=params.model_num_heads, agent_history=params.agent_history)
+
     optimizer = torch.optim.Adam(Q_network.parameters(), lr=params.adam_learning_rate, eps=params.adam_epsilon)
 
     # parallelism if multiple GPUs
-    #if torch.cuda.device_count() > 1:
+    # if torch.cuda.device_count() > 1:
     #    Q_network = nn.DataParallel(Q_network)
     #    Q_target_network = nn.DataParallel(Q_target_network)
 
@@ -56,19 +61,19 @@ def train(params):
     # initiate training
     print(f"\nStarting Training\nEpochs: {params.epochs}\nIterations per Epoch: {params.iterations}\n\n")
     for epoch in range(params.epochs):
-        
+
         sp.increment_and_print("epoch")
         sp.print_statement("iter")
         sp.reset_element("iter")
 
         for iteration in range(1, params.iterations + 1):
-            
+
             sp.increment_and_print("iter")
             train_loss = agent.train_batch()
-            
+
             if iteration % params.iter_target_update == 0:
                 agent.update_target()
-            
+
             if iteration % params.iter_buffer_update == 0:
                 agent.replay_buffer.load_new_buffer()
 
@@ -83,10 +88,11 @@ def train(params):
         total_action_freq = np.zeros(num_actions)
         for run in range(params.validation_runs):
             sp.increment_and_print("valid")
-            online_reward, action_freq = online_validation(agent=agent, env=env, max_step_count=params.agent_max_val_steps)
+            online_reward, action_freq = online_validation(agent=agent, env=env,
+                                                           max_step_count=params.agent_max_val_steps)
             total_reward += online_reward
             total_action_freq += action_freq
-            
+
         validation_reward = total_reward/params.validation_runs
         total_action_freq /= params.validation_runs
 
@@ -100,25 +106,26 @@ def online_validation(agent, env, max_step_count, render=False):
 
     step_count = 0
     total_reward = 0
-    
+
     done = False
     state = env.reset()
     state = torch.from_numpy(state).float()
-    state = torch.reshape(state, (1,1,state.shape[0], state.shape[1]))
+    state = torch.reshape(state, (1, 1, state.shape[0], state.shape[1]))
     agent.state_buffer.reset(state)
 
     freq_actions = np.zeros(env.action_space.n)
 
     while not done and step_count < max_step_count:
         action = agent.act(state, deterministic=False)
-        state, reward, done, _ = env.step(action)  #TODO: does the input have to be a tuple
+        state, reward, done, _ = env.step(action)
         state = torch.from_numpy(state).float()
         state = torch.reshape(state, (1, 1, state.shape[0], state.shape[1]))
         freq_actions[action] += 1
+
         if render:
             time.sleep(0.03)
             env.render("human")
-        
+
         total_reward += reward
         step_count += 1
 
