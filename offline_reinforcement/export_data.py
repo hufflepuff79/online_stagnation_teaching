d1c348d2cdcece056bf2aeb5061d5ffdfbfd4f3b
LuggiStruggi <lukasmkoenig@gmx.net>
2021-07-23 15:59:35 +0200

    fix export data

--- offline_reinforcement/export_data.py
+++ offline_reinforcement/export_data.py
@@ -17,11 +17,11 @@ def export_and_save_data(dir_in, dir_out, datapoints = None):
         for e in raw_dataset:
             datapoints += 1
 
-    data = {'observations' : np.zeros((datapoints, 17)),
-            'next_observations' : np.zeros((datapoints, 17)),
-            'rewards' : np.zeros(datapoints),
-            'actions' : np.zeros((datapoints, 6)),
-            'terminals' : np.zeros(datapoints)}
+    data = {'observations' : np.zeros((datapoints, 17), dtype=np.float32),
+            'next_observations' : np.zeros((datapoints, 17), dtype=np.float32),
+            'rewards' : np.zeros(datapoints, dtype=np.float32),
+            'actions' : np.zeros((datapoints, 6), dtype=np.float32),
+            'terminals' : np.zeros(datapoints, dtype=np.bool)}
 
     c = 0
     for raw_record in raw_dataset:
@@ -33,7 +33,7 @@ def export_and_save_data(dir_in, dir_out, datapoints = None):
         data['next_observations'][c, :] = np.concatenate((sns_p[8:], sns_v[9:]))
         data['actions'][c, :] = np.array(example.features.feature['action'].float_list.value)[:6]
         data['rewards'][c] = np.array(example.features.feature['reward'].float_list.value[0])
-    c += 1
+        c += 1
 
     a_file = open(os.path.join(dir_out, "data.pkl"), "wb")
     pickle.dump(data, a_file)
