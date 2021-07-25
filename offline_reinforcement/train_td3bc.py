a42d4ba224892267294c3cf61c88435c145a2009
LuggiStruggi <lukasmkoenig@gmx.net>
2021-07-25 15:05:11 +0200

    some cleanup

--- offline_reinforcement/train_td3bc.py
+++ offline_reinforcement/train_td3bc.py
@@ -11,7 +11,7 @@ import numpy as np
 from os.path import exists, join
 from os import makedirs
 import wandb
-# from dm_control import suite
+from dm_control import suite
 
 import time
 
@@ -36,54 +36,34 @@ def train(params, seed: int = 42, log_wb: bool = False, logging_freq: int = 1000
     if not exists(log_dir):
         makedirs(log_dir)
 
-    if params.env_type == 'dm_control':
-        # Import correct libs
-        from dm_control import suite, viewer
-        # get the envirionment infos
-        if params.env_name == 'cheetah':
-            env = suite.load('cheetah', 'run')
-            action_space = env.action_spec().shape[0]
-            max_action = torch.from_numpy(env.action_spec().maximum.astype(np.float32)).to(device)
-            min_action = torch.from_numpy(env.action_spec().minimum.astype(np.float32)).to(device)
-            observation_space = 17
-            env.close()
-        elif params.env_name == 'humanoid':
-            env = suite.load('humanoid', 'run')
-            action_space = env.action_spec().shape[0]
-            max_action = torch.from_numpy(env.action_spec().maximum.astype(np.float32)).to(device)
-            min_action = torch.from_numpy(env.action_spec().minimum.astype(np.float32)).to(device)
-            observation_space = 67
-            env.close()
-        else:
-            print('Unsupported env_name for gym envirionment. Use cheetah or humanoid')
-            return
-        
-        # Load the dataset, using_d4rl=using_d4rl
-        try:
-            path = ''
-            path = join(params.data_dir, params.env_name + '_data.pkl')
-            with open(path, 'rb') as f:
-                dataset = pickle.load(f)
-        except Exception as e:
-            print(e)
-            print(f'Unable to load the pickled data from {path}')
-            return
-    elif params.env_type == 'gym':
-        import d4rl
-        import gym
-        if params.env_name == 'cheetah':
-            env = gym.make('halfcheetah-expert-v2')
-        else:
-            print('Unsupported env_name for gym envirionment. Use cheetah')
-            return
-        dataset = d4rl.qlearning_dataset(env)
-        action_space = env.action_space.shape[0]
-        max_action = torch.from_numpy(env.action_space.high).to(device)
-        min_action = torch.from_numpy(env.action_space.low).to(device)
-        observation_space = env.observation_space.shape[0]
+    # Import correct libs
+    if params.env_name == 'cheetah':
+        env = suite.load('cheetah', 'run')
+        action_space = env.action_spec().shape[0]
+        max_action = torch.from_numpy(env.action_spec().maximum.astype(np.float32)).to(device)
+        min_action = torch.from_numpy(env.action_spec().minimum.astype(np.float32)).to(device)
+        observation_space = 17
+        env.close()
+    elif params.env_name == 'humanoid':
+        env = suite.load('humanoid', 'run')
+        action_space = env.action_spec().shape[0]
+        max_action = torch.from_numpy(env.action_spec().maximum.astype(np.float32)).to(device)
+        min_action = torch.from_numpy(env.action_spec().minimum.astype(np.float32)).to(device)
+        observation_space = 67
         env.close()
     else:
-        print('Unsupported environment type. Use dm_control or gym.')
+        print('Unsupported env_name for gym envirionment. Use cheetah or humanoid')
+        return
+    
+    # Load the dataset
+    try:
+        path = ''
+        path = join(params.data_dir, params.env_name + '_data.pkl')
+        with open(path, 'rb') as f:
+            dataset = pickle.load(f)
+    except Exception as e:
+        print(e)
+        print(f'Unable to load the pickled data from {path}')
         return
 
     # create networks 
@@ -135,7 +115,7 @@ def train(params, seed: int = 42, log_wb: bool = False, logging_freq: int = 1000
 
         sp.print_statement("valid")
         sp.reset_element("valid")
-        total_reward = online_validation(agent=agent, env_name=params.env_name, env_type=params.env_type, seed=seed, num_episodes=params.validation_runs, status_func=sp.increment_and_print,
+        total_reward = online_validation(agent=agent, env_name=params.env_name, seed=seed, num_episodes=params.validation_runs, status_func=sp.increment_and_print,
                                         status_arg="valid")
         sp.done_element("valid")
       
@@ -148,84 +128,51 @@ def train(params, seed: int = 42, log_wb: bool = False, logging_freq: int = 1000
             agent.save(log_dir, epoch)
 
 
-def online_validation(agent, env_name, env_type, seed=42, num_episodes=10, status_func=lambda *args :None, status_arg=None, render=False):
-    if env_type == 'dm_control':
-        # Import correct libs
-        from dm_control import suite, viewer
-        # get the envirionment infos
-        if env_name == 'cheetah':
-            env = suite.load('cheetah', 'run', task_kwargs={'random' : seed})
-        elif env_name == 'humanoid':
-            env = suite.load('humanoid', 'run', task_kwargs={'random' : seed})
-        else:
-            print('Unsupported env_name for gym envirionment. Use cheetah or humanoid')
-            return
-    elif env_type == 'gym':
-        import d4rl
-        import gym
-        if env_name == 'cheetah':
-            env = gym.make('halfcheetah-expert-v2')
-            env.seed(seed)
-            env.action_space.seed(seed)
-        else:
-            print('Unsupported env_name for gym envirionment. Use cheetah')
-            return
+def online_validation(agent, env_name, seed=42, num_episodes=10, status_func=lambda *args :None, status_arg=None, render=False):
+    if env_name == 'cheetah':
+        env = suite.load('cheetah', 'run', task_kwargs={'random' : seed})
+    elif env_name == 'humanoid':
+        env = suite.load('humanoid', 'run', task_kwargs={'random' : seed})
     else:
-        print('Unsupported environment type. Use dm_control or gym.')
+        print('Unsupported env_name for gym envirionment. Use cheetah or humanoid')
         return
-
+    
     total_reward = 0
     for _ in range(num_episodes):
+        
         done = False
-        if env_type == 'gym':
-            state = env.reset()
-            state = (state-agent.replay_buffer.mean)/agent.replay_buffer.std
-            state = torch.from_numpy(state).float()
+        unplugged_rl_step_count = 0
+        time_step = env.reset()
+        if env_name == "cheetah":
+            state = np.concatenate((time_step.observation['position'], time_step.observation['velocity']))
         else:
-            unplugged_rl_step_count = 0
-            time_step = env.reset()
+            state = np.concatenate((time_step.observation['joint_angles'], np.expand_dims(np.array(time_step.observation['head_height']), axis=0),
+                                    time_step.observation['extremities'], time_step.observation['torso_vertical'],
+                                    time_step.observation['com_velocity'], time_step.observation['velocity']))
+
+        state = (state-agent.replay_buffer.mean)/agent.replay_buffer.std
+        state = torch.from_numpy(state).float()
+
+        while not done:
+            action = agent.act(state)
+            time_step = env.step(action)
+
             if env_name == "cheetah":
                 state = np.concatenate((time_step.observation['position'], time_step.observation['velocity']))
             else:
                 state = np.concatenate((time_step.observation['joint_angles'], np.expand_dims(np.array(time_step.observation['head_height']), axis=0),
-                                        time_step.observation['extremities'], time_step.observation['torso_vertical'],
-                                        time_step.observation['com_velocity'], time_step.observation['velocity']))
+                                    time_step.observation['extremities'], time_step.observation['torso_vertical'],
+                                    time_step.observation['com_velocity'], time_step.observation['velocity']))
 
             state = (state-agent.replay_buffer.mean)/agent.replay_buffer.std
             state = torch.from_numpy(state).float()
-
-        while not done:
-            action = agent.act(state)
-            if env_type == 'gym':
-                state, reward, done, _ = env.step(action)
-                state = (state-agent.replay_buffer.mean)/agent.replay_buffer.std
-                state = torch.from_numpy(state).float()
-                if render:
-                    env.render()
-                    time.sleep(0.01)
-            else:
-                time_step = env.step(action)
-                if render:
-                    viewer.launch(env, agent.render_policy)
-
-                if env_name == "cheetah":
-                    state = np.concatenate((time_step.observation['position'], time_step.observation['velocity']))
-                else:
-                    state = np.concatenate((time_step.observation['joint_angles'], np.expand_dims(np.array(time_step.observation['head_height']), axis=0),
-                                        time_step.observation['extremities'], time_step.observation['torso_vertical'],
-                                        time_step.observation['com_velocity'], time_step.observation['velocity']))
-
-                state = (state-agent.replay_buffer.mean)/agent.replay_buffer.std
-                state = torch.from_numpy(state).float()
-                reward = time_step.reward
-                unplugged_rl_step_count += 1
-                if unplugged_rl_step_count >= 1000:
-                    done = True
+            reward = time_step.reward
+            unplugged_rl_step_count += 1
+            if unplugged_rl_step_count >= 1000:
+                done = True
             total_reward += reward
             status_func(status_arg)
     total_reward /= num_episodes
-    if env_type == 'gym':
-        total_reward = env.get_normalized_score(total_reward)
 
     env.close()
     return total_reward
@@ -233,7 +180,6 @@ def online_validation(agent, env_name, env_type, seed=42, num_episodes=10, statu
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Parser to Initiate Agent Training")
     parser.add_argument('--env_name', type=str, default='cheetah', help='Name of the environment (cheetah or human)')
-    parser.add_argument('--env_type', type=str, default='gym', help='Type of the environment (dm_control or gym)')
     parser.add_argument('--data_dir', type=str, help='Path to the transition data of the unplugged_rl dataset')
     parser.add_argument('--epochs', type=int, help='Number of test evaluations. Every epoch there are <iterations> iterations')
     parser.add_argument('--iterations', type=int, help='aNumber of iterations per epoch. Agent gets and trains one batch per epoch')
